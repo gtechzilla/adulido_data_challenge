e2d8693c8bf54fa2e341507b4edc4aad79e58e9e
gtechzilla <okiomagerald@gmail.com>
2020-12-19 14:54:15 +0300

    made changes to test file

--- test/test_set1.py
+++ test/test_set1.py
@@ -2,31 +2,19 @@ import unittest
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_equal, assert_string_equal
 from numpy.testing import assert_allclose, assert_raises, assert_raises_regex
-import pytest
+import json
+import requests
 
     
 def test_basic_test():
     out = {}
     assert_equal(isinstance(out, dict), True)
-    
-def test_for_checkbox_values():
-    '''
-    This tests to check if the values from the incoming call translates to the query.
-    It checks for the length of the list as well as the values. 
-    '''
-    assert len(input_values) == len(checkbox_values)
-    assert x: for x in checkbox_values is in input_values
 
-def test_for_data_filter():
-    '''
-    Tests if the data displayed is as the data requested.
-    '''
-    assert data_displayed == data[query]
+
 
 def test_for_end_API():
     '''
-    This is to ensure that the end API is displayed with content choosen. 
-    Even if there is no filter applied all data is displayed
+    test to see if we can get data to our API
     '''
-    result = client.post('/recommend_sort_games/', data=json.dumps(data), content_type='application/json',)
+    result = requests.post('/recommend_sort_games/', data=json.dumps(data), content_type='application/json',)
     assert result.json is not None 
